6d0ddeff88fc478c7cf9a58a89b6f3b5f8857ba8
NicolasISEN <nicolas.thuylie@isen.yncrea.fr>
2020-02-10 11:08:11 +0100

    WIP Endpoint

--- lab05-server/src/main/java/yncrea/lab05/web/config/WSconfig.java
+++ lab05-server/src/main/java/yncrea/lab05/web/config/WSconfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import yncrea.lab05.contract.facade.CompanyWS;
-import yncrea.lab05.web.service.impl.CompanyWSImpl;
 
 
 @Configuration
--- lab05-server/src/main/java/yncrea/lab05/web/service/impl/CompanyWSImpl.java
+++ lab05-server/src/main/java/yncrea/lab05/web/service/impl/CompanyWSImpl.java
@@ -2,21 +2,42 @@ package yncrea.lab05.web.service.impl;
 
 import yncrea.lab05.contract.dto.CompanyDTO;
 import yncrea.lab05.contract.facade.CompanyWS;
+import yncrea.lab05.core.dao.CompanyDAO;
+import yncrea.lab05.core.entity.Company;
+import yncrea.lab05.core.service.CompanyService;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.jws.WebService;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Named("companyWS")
 @WebService(endpointInterface = "yncrea.lab05.contract.facade.CompanyWS")
 public class CompanyWSImpl implements CompanyWS {
-    @Override
-    public Collection<CompanyDTO> getAllCompanies() {
-        return null;
-    }
+
+    @Inject
+    private CompanyService companyService;
+
 
     @Override
     public void saveCompany(CompanyDTO companyDTO) {
-
+        Company company = new Company(companyDTO.getName());
+        this.companyService.save(company);
     }
+
+    @Override
+    public Collection<CompanyDTO> getAllCompanies() {
+        List<CompanyDTO> companyDTOS= new ArrayList<>();
+        List<Company> companies = this.companyService.findAllWithProjects();
+
+        companies.forEach(company -> {
+            companyDTOS.add(new CompanyDTO(company.getName()));
+        });
+
+        return companyDTOS;
+    }
+
+
 }
